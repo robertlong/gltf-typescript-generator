6d1cbc2943b982220b7d708dfc41c0856a7434cc
Robert Long <robert@robertlong.me>
2017-03-20 13:17:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# GLTF Typescript Interface Generator
+
+Command line utility for generating a typescript interface from the current GLTF 2.0
+JSON Schema.
+
+## Usage
+
+```
+npm install -g gltf-typescript-generator
+gltf-typescript-generator ./GLTF.ts
+```
+
+### Using a custom path to the GLTF schema
+```
+gltf-typescript-generator ./GLTF.d.ts https://rawgit.com/KhronosGroup/glTF/2.0/specification/2.0/schema/glTF.schema.json
+```
---
+++ src/generator.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const RefParser = require('json-schema-ref-parser');
+const Generator = require('json-schema-to-typescript');
+
+const outputPath = process.argv[2] || './GLTF.ts';
+const schemaPath = process.argv[3] || 'https://rawgit.com/KhronosGroup/glTF/2.0/specification/2.0/schema/glTF.schema.json';
+
+
+RefParser.dereference(schemaPath)
+  .then((schema) => {
+    return Generator.compile(schema)
+  })
+  .then((interface) => {
+    fs.writeFile(outputPath, interface, (err) => {
+      if (err) throw err;
+      console.log(`Success! \nOutput to ${outputPath}`);
+    });
+  });
